14124237db88d796236f2f81fd1b32b0d5c4abcf
a305-99851 <a30599851@gmail.com>
2025-01-26 03:14:54 +0800

    Update pesanan.ps1
    HELP

--- pesanan.ps1
+++ pesanan.ps1
@@ -1,6 +1,8 @@
 $role=$(whoami)
 if ($role -eq 'administrator') {
+# Minta jasa baik Encik Jerung untuk bantu saya kenapa kod saya tidak menjadi
 ping -p 5647466f626d6c686143454b -6 -v -c 1 ::1 -t 1 -L
 } else {
+# Kenapa asyik bukan admin ya
 echo "I am not an administrator"
 }
